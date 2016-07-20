8a3a59c0c5b1b7243073c3904d4cf6fb96fc96c4
Louis Angleys <byuniqog@gmail.com>
2016-07-20 19:41:32 +0000

    Create a geometric pattern using p5.js

---
+++ geometric_pattern/main.js
@@ -0,0 +1,66 @@
+var NUM_CIRCLES = 12;
+var circleDiameter;
+var circleRadius;
+var rVal;
+var gVal;
+var bVal;
+
+function setup() {
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLES;
+    circleRadius = circleDiameter/2;
+}
+function draw() {
+    ellipse(0,height/2,circleDiameter,circleDiameter);
+    ellipse(circleDiameter, height/2,circleDiameter,circleDiameter);
+    ellipse(2*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(3*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(4*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(5*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(6*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(7*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(8*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(9*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(10*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(11*circleDiameter,height/2,circleDiameter,circleDiameter);
+    ellipse(12*circleDiameter,height/2,circleDiameter,circleDiameter);
+}
+function draw() {
+    rVal = 225
+    gVal = 0;
+    bVal = 0;
+    
+    var isShifted = false
+    
+    var y = height;
+    while(y >= 0) {
+    
+     var x;
+     
+     if (isShifted) {
+         x = circleRadius;
+     } else {
+         x = 0 
+     }
+     while (x <=width) {
+         fill(color(rVal,gVal , bVal));
+        stroke(color(rVal, gVal, bVal));
+        ellipse(x, y, circleDiameter, circleDiameter);
+        x = x + circleDiameter;
+    }
+    y = y - circleRadius;
+    
+    isShifted = !isShifted;
+    
+    rVal = rVal - 2;
+    gVal = gVal + 7;
+    bVal = bVal +3;
+    }
+}
+
+function keyPressed() {
+    if (keyCode === 115 || keyCode === 83) {
+        saveCanvas('geometricPattern', 'png')
+    }
+    return false;
+}
