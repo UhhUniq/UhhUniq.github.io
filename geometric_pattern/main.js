a8e3ab36c71342b414b646ed1d0b50d5b2a831ed
Louis Angleys <byuniqog@gmail.com>
2016-07-20 19:52:05 +0000

    Create a geometric pattern using p5.js

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,34 +1,40 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
     createCanvas(480, 600);
+    
+    frameRate(5)
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius = circleDiameter/2;
-}
-function draw() {
-    ellipse(0,height/2,circleDiameter,circleDiameter);
-    ellipse(circleDiameter, height/2,circleDiameter,circleDiameter);
-    ellipse(2*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(3*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(4*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(5*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(6*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(7*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(8*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(9*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(10*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(11*circleDiameter,height/2,circleDiameter,circleDiameter);
-    ellipse(12*circleDiameter,height/2,circleDiameter,circleDiameter);
-}
-function draw() {
-    rVal = 225
+    
+    rVal = 225;
     gVal = 0;
     bVal = 0;
+}
+// function draw() {
+//     ellipse(0,height/2,circleDiameter,circleDiameter);
+//     ellipse(circleDiameter, height/2,circleDiameter,circleDiameter);
+//     ellipse(2*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(3*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(4*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(5*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(6*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(7*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(8*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(9*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(10*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(11*circleDiameter,height/2,circleDiameter,circleDiameter);
+//     ellipse(12*circleDiameter,height/2,circleDiameter,circleDiameter);
+// }
+function draw() {
     
     var isShifted = false
     
@@ -52,9 +58,9 @@ function draw() {
     
     isShifted = !isShifted;
     
-    rVal = rVal - 2;
-    gVal = gVal + 7;
-    bVal = bVal +3;
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal +3) %256;
     }
 }
 
